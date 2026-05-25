06e3b6881e1d01d1c2ccce37ea3eb37cf504d563
rajaiteam <rajkumaralltype@gmail.com>
2026-05-26 01:18:28 +0530

    Update callback URL in server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/pay", async(req,res)=>{
               "ORD_" + Date.now(),
 
             callback_url:
-              "https://paymentuu.onrender.com"
+              "https://paymentuu.onrender.com/callback"
          }
       );
 
